827e07e8c1b0409293b0d7b05b89ed597a6540b8
kanbertasli <kanber.tasli@nevakontrol.com>
2020-12-21 23:08:47 +0300

    Add files via upload

---
+++ Cost_of_Meal.py
@@ -0,0 +1,7 @@
+y= int(input("Please enter cost of meal: "))
+
+
+print (f'Cost of the meal is {y} Eur')
+
+print (f'The tax is {y*0.08} Eur and the tip is {y*0.10} Eur, making the total {y*1.18} Eur')
+
